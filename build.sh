2c4c532dfffc9d886afd3228c38db318d816cac3
Florian Brinker <mail+gitlab@f-brinker.de>
2021-04-21 23:39:23 +0200

    Add published version/tag check to skip docker builds

--- build.sh
+++ build.sh
@@ -10,20 +10,35 @@ getVersionFromLatestRelease() {
 }
 
 downloadLatestRelease() {
-    URL=`curl -s "https://api.github.com/repos/resoai/TileBoard/releases/latest" \
+    url=`curl -s "https://api.github.com/repos/resoai/TileBoard/releases/latest" \
         | grep "browser_download_url" \
         | cut -d '"' -f 4`
-    echo "Url: $URL"
+    echo "Url: $url"
     
-    curl -sL -o $1 ${URL}
+    curl -sL -o $1 ${url}
 }
 
-LATEST=`getVersionFromLatestRelease`
-SEMVER=( ${LATEST//./ } )
+getLatestPublishedTag() {
+    latest_tag=`curl -s "https://hub.docker.com/v2/repositories/fbrinker/tileboard/tags?page_size=1" \
+        | jq -r ".results[0].name"`
 
+    echo "$latest_tag"
+}
+
+LATEST_RELEASE=`getVersionFromLatestRelease`
+LATEST_TAG=`getLatestPublishedTag`
+
+if [ "$LATEST_RELEASE" = "$LATEST_TAG" ]; then
+    echo "Nothing to do. Versions already match."
+    echo "Release: $LATEST_RELEASE"
+    echo "Tag: $LATEST_TAG"
+    exit 78 # drone.io exit code to stop but success the pipeline
+fi
+
+SEMVER=( ${LATEST_RELEASE//./ } )
 MAJOR=${SEMVER[0]}
 MINOR=${SEMVER[0]}.${SEMVER[1]}
-PATCH=$LATEST
+PATCH=$LATEST_RELEASE
 
 echo "latest,$MAJOR,$MINOR,$PATCH" > .tags
 
